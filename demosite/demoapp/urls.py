db213a07ae673d8d74f62946fde71104d372ed09
Antony Jose <antonyjose777aj@gmail.com>
2026-02-24 13:27:19 +0530

    class based View test + decorator for class

--- demosite/demoapp/urls.py
+++ demosite/demoapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .views import Myview
 
 urlpatterns= [
     path('register/',views.register_user),
     path('',views.home,name='home'),
     path('checkpost/',views.check_post,name='check_post'),
     path('delete/<int:item_id>/',views.delete_post,name='delete_post'),
+    path('classbasedview/',Myview.as_view(),name='Myview')
 ]
 
--- demosite/demoapp/views.py
+++ demosite/demoapp/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse, HttpResponseNotAllowed
 import threading
 import time
 from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from django.utils.decorators import method_decorator
+import json
 def send_email(user,email):
     time.sleep(5)
     print("email send.")
@@ -31,3 +34,20 @@ def delete_post(request,item_id):
         # item_id = request.GET.get("id") #query parameter style , use rest style-> industry standard.
         return HttpResponse(f"Deleted item :{item_id}")
     return HttpResponseNotAllowed(['DELETE'])
+
+@method_decorator(csrf_exempt,name='dispatch')
+class Myview(View):
+    def get(self,request):
+        return HttpResponse("this is a get request managed by django automatically.")
+    
+    def post(self,request):
+        return HttpResponse("this is a post request managed by django automatically.")
+    
+    def put(self,request):
+        body = request.body
+        data= json.loads(body)
+        return HttpResponse(f"data : {data}")
+    
+    def delete(self,request):
+        return HttpResponse(f"delete request recived.")
+    
