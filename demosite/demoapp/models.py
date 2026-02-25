de0ef4fcee32f80176fda570f03fb5fec1aae5c6
Antony Jose <antonyjose777aj@gmail.com>
2026-02-25 11:33:14 +0530

    Test cases Student model + commands file

--- demosite/demoapp/models.py
+++ demosite/demoapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    description = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    
--- demosite/demoapp/tests.py
+++ demosite/demoapp/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Student
 
 # Create your tests here.
+
+class StudentModelTest(TestCase):
+    def setUp(self): #case sensitive 
+        self.student1  = Student.objects.create(
+            name = "Antony",
+            email = "antony@example.com"
+        )
+
+    def teststudentcreation(self):
+        self.assertEqual(self.student1.name,"Antony")
+        self.assertEqual(self.student1.email,"antony@example.com")
+        self.assertIsNotNone(self.student1.created_at)
+
+    def test_str_method(self):
+        self.assertEqual(str(self.student1),"Antony")
+
+    def test_email_unique(self):
+        with self.assertRaises(Exception):
+            Student.objects.create(
+                name = "kuku",
+                email = "antony@example.com"
+            )
