930076297d68efc0b535e19b8ac1bcd80157e2b6
Antony Jose <antonyjose777aj@gmail.com>
2026-03-02 22:46:47 +0530

    conditional statements in html + important operators need space around them (orwill be considered as a token -error)

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_view(request):
-    context = {"name":"antony"}
+    context = {"name":"antony","age":20}
     return render(request,"shop/welcome.html",context)
 
 def home_view(request):
-    return render(request,"shop/home.html")
+    context = {"value":5000,"age":25}
+    return render(request,"shop/home.html",context)
