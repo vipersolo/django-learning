46022f3d9a469904a281f0515d0718f8c5491e42
Antony Jose <antonyjose777aj@gmail.com>
2026-03-05 20:06:52 +0530

    roll based access +base.html

--- companysite/employee/urls.py
+++ companysite/employee/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/',views.login_view,name='login'),
     path('home/',views.home_view,name='home'),
     path('logout/',views.logout_view,name='logout'),
+    path('admin-dashboard/',views.admin_dashboard,name="admin_dashboard"),
+    path('manager_dashboard/',views.manager_dashboard,name="manager_dashboard"),
+    path('employee_dashboard/',views.employee_dashboard,name="employee_dashboard"),
 ]
--- companysite/employee/views.py
+++ companysite/employee/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User,Group
+from django.contrib.auth.decorators import user_passes_test
 # Create your views here.
 def register_view(request):
     if request.method == 'POST':
@@ -32,3 +34,34 @@ def home_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+user = User.objects.get(username="john")
+group = Group.objects.get(name="Manager")
+user.groups.add(group)
+
+user = User.objects.get(username="employee1")
+group = Group.objects.get(name="Employee")
+user.groups.add(group)
+
+user = User.objects.get(username="root")
+group = Group.objects.get(name="Admin")
+user.groups.add(group)
+
+def is_admin(user):
+    return user.groups.filter(name="Admin").exists()
+def is_manager(user):
+    return user.groups.filter(name="Manager").exists()
+def is_employee(user):
+    return user.groups.filter(name="Employee").exists()
+
+@user_passes_test(is_admin)
+def admin_dashboard(request):
+    return render(request,"employee/admin_dashboard.html")
+
+@user_passes_test(is_manager)
+def manager_dashboard(request):
+    return render(request,"employee/manager_dashboard.html")
+
+@user_passes_test(is_employee)
+def employee_dashboard(request):
+    return render(request,"employee/employee_dashboard.html")
