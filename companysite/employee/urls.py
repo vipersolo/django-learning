77a713fec16d41bebe93c209956f1c665fe52941
Antony Jose <antonyjose777aj@gmail.com>
2026-03-05 13:37:03 +0530

    new project company_site + full login-logout

---
+++ companysite/employee/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/',views.register_view,name='register'),
+    path('login/',views.login_view,name='login'),
+    path('home/',views.home_view,name='home'),
+    path('logout/',views.logout_view,name='logout'),
+]
---
+++ companysite/employee/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'employee/register.html',{'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request,'employee/login.html',{'form':form})
+
+
+@login_required
+def home_view(request):
+    return render(request,'employee/home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
